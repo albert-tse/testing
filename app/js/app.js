b2fcf05e53f581de425dac13b93aba1f6bbe6ec8
James Tymann <jtymann@the-social-edge.com>
2016-04-20 17:16:49 -0400

    Added react-router

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,12 @@
 import React from 'react';
-
+import { Router, Route, Link, hashHistory } from 'react-router'
 import App from './components/app';
 import Alt from './alt';
 
-React.render(<App />, document.getElementById('app-container'));
+/*render(<App />, document.getElementById('app-container'));*/
+
+React.render(
+    <Router history={hashHistory}>
+    <Route path="/" component={App}>
+    </Route>
+  </Router>, document.getElementById('app-container'))
