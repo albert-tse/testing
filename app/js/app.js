007ed801c11c18ef32eb27e6bd41edefea5c005a
James Tymann <jtymann@the-social-edge.com>
2016-04-21 19:47:44 -0400

    Disabled query keys with hash history.

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom'
-import { Router, Route, Link, hashHistory } from 'react-router'
+import { Router, Route, Link, useRouterHistory } from 'react-router'
 import Alt from './alt';
+import { createHashHistory } from 'history'
+const hashHistory = useRouterHistory(createHashHistory)({ queryKey: false })
 
 import Legacy from './components/legacy';
 import Login from './components/login';
+
+console.log(hashHistory);
 
 render(
     <Router history={hashHistory}>
