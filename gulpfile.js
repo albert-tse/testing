b32167446fb44682c9655251329f1eeed4f9bb94
Ken Krug <ken@the-social-edge.com>
2016-05-04 13:43:29 -0400

    Fixed arbitrary ordering of SCSS source inject

--- gulpfile.js
+++ gulpfile.js
@@ -22,6 +22,7 @@ var argv = require('yargs').argv;
 var wiredep = require('wiredep').stream;
 var clean = require('gulp-clean');
 var open = require('gulp-open');
+var naturalSort = require('gulp-natural-sort');
 
 var CacheBuster = require('gulp-cachebust');
 var cachebust = new CacheBuster();
@@ -139,7 +140,7 @@ gulp.task('inject', function () {
     return gulp.src('./' + appPath + '/scss/app.scss')
         .pipe(
             inject(
-                sources, {
+                sources.pipe(naturalSort()), {
                     relative: true,
                     empty: true
                 }
