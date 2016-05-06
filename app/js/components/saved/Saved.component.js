b195d1f78b97edc66705b656cbe48a5ee8bcf140
Albert Villaroman <albert@the-social-edge.com>
2016-05-06 16:46:52 -0400

    moved: displaying actions logic outside of article

--- app/js/components/saved/Saved.component.js
+++ app/js/components/saved/Saved.component.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header, Toolbar } from '../shared';
+import { Header } from '../shared';
 import Article from '../shared/Article.component';
 
 
@@ -7,25 +7,40 @@ class Saved extends React.Component {
 
     constructor(props) {
         super(props);
-        console.log('Saved props are: ', this.props);
     }
 
     render() {
         return (
-        <div id="saved" className="tab-content">
+        <div id="saved-tab" className="tab-content">
             <Header />
-            <Toolbar />
             <div id="articles-container" className="container-fluid row">
-                {this.props.articles.map(this.renderArticle)}
+                {this.props.articles.map(::this.renderArticle)}
             </div>
         </div>
         );
     }
 
     renderArticle(article, index) {
-        return <Article key={index} data={article} />;
+        return (
+            <Article key={index} data={article}>
+                <actions>
+                    <action position="left"><a className="highlight-on-hover" href={ '/?relatedto=' + article.ucid}>Related</a></action>
+                    <action position="left">
+                        <div className="btn-group">
+                            <a type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" className="highlight-on-hover" href="#share">Share</a>
+                            <ul className="dropdown-menu">
+                                <li><a className="social-btn" data-platform="Facebook" data-url={article.url} data-ucid={article.ucid} data-platform-url><i className="fa fa-facebook"></i> Facebook</a></li>
+                                <li><a className="social-btn" data-platform="Twitter" data-url={article.url} data-ucid={article.ucid} data-platform-url><i className="fa fa-twitter"></i> Twitter</a></li>
+                                <li><a className="social-btn" data-platform="Tumblr" data-url={article.url} data-description={article.description} data-title={article.title} data-ucid={article.ucid} data-platform-url><i className="fa fa-tumblr"></i> Tumblr</a></li>
+                                <li><a className="social-btn" data-platform="Pinterest" data-url={article.url} data-image={article.image} data-description={article.description} data-ucid={article.ucid} data-platform-url><i className="fa fa-pinterest"></i> Pinterest</a></li>
+                                <li><a className="social-btn" data-platform="Google +" data-url={article.url} data-ucid={article.ucid} data-platform-url><i className="fa fa-google-plus"></i> Google+</a></li>
+                            </ul>
+                        </div>
+                    </action>
+                </actions>
+            </Article>
+        );
     }
-    
 }
 
 export default Saved;
--- app/js/components/shared/Article.component.js
+++ app/js/components/shared/Article.component.js
@@ -10,7 +10,6 @@ class Article extends React.Component {
 
     constructor(props) {
         super(props);
-        console.log(this.props.data);
     }
 
     render() {
@@ -18,32 +17,16 @@ class Article extends React.Component {
 
         if ('data' in this.props) {
             return (
-                <div id={ 'article-' + article.ucid } className="grid-item article articlex" data-ucid={article.ucid}>
+                <div id={ 'article-' + article.ucid } className="grid-item article articlex highlight-on-hover" data-ucid={article.ucid}>
                     <img className="th" src={article.image} />
                     <div className="metadata">
-                        <a className="info" ucid={article.ucid}><i className="fa fa-info-circle fa-lg"></i></a>
+                        <a className="info highlight-on-hover" ucid={article.ucid}><i className="fa fa-info-circle fa-lg"></i></a>
                         <time datetime={article.createdAt.format()}>{article.createdAt.fromNow()}</time>
                         <span className="site"> by {article.siteId} rated {'M'}</span>
                     </div>
-                    <h1 className="headline">{article.title}</h1>
+                    <h1 className="headline highlight-on-hover">{article.title}</h1>
                     <p className="description">{article.description}</p>
-                    <div className="actions">
-                        <div className="left action">
-                            <a href="#related">Related</a>
-                        </div>
-                        <div className="share left action highlight-on-hover">
-                            <div className="btn-group">
-                                <a type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" className="highlight-on-hover" href="#share">Share</a>
-                                <ul className="dropdown-menu">
-                                    <li><a className="social-btn" data-platform="Facebook" data-url={article.url} data-ucid={article.ucid} data-platform-url><i className="fa fa-facebook"></i> Facebook</a></li>
-                                    <li><a className="social-btn" data-platform="Twitter" data-url={article.url} data-ucid={article.ucid} data-platform-url><i className="fa fa-twitter"></i> Twitter</a></li>
-                                    <li><a className="social-btn" data-platform="Tumblr" data-url={article.url} data-description={article.description} data-title={article.title} data-ucid={article.ucid} data-platform-url><i className="fa fa-tumblr"></i> Tumblr</a></li>
-                                    <li><a className="social-btn" data-platform="Pinterest" data-url={article.url} data-image={article.image} data-description={article.description} data-ucid={article.ucid} data-platform-url><i className="fa fa-pinterest"></i> Pinterest</a></li>
-                                    <li><a className="social-btn" data-platform="Google +" data-url={article.url} data-ucid={article.ucid} data-platform-url><i className="fa fa-google-plus"></i> Google+</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+                    {::this.renderActions()}
                 </div>
             );
         }
@@ -52,7 +35,66 @@ class Article extends React.Component {
             <div />
         );
     }
+
+    /**
+     * Render any call to actions here
+     * Actions are passed via children elements
+     * The reason we do this is so that we can loosely-couple the logic of specifying which actions to show
+     * @return React.DOM
+     */
+    renderActions() {
+        var actions = this.getActionElements();
+
+        // If actions are specified, render
+        if (actions.length > 0) {
+            return (
+                <div className="actions">
+                    {actions.map(function (action, index) {
+                        return <Action key={index} {...action.props}>{action.props.children}</Action>;
+                    })}
+                </div>
+            );
+        }
+    }
+
+   /**
+    * Get the action elements nested inside the Article component
+    * @return Array of action elements
+    */
+    getActionElements() {
+        var actions = [];
+        if (Array.isArray(this.props.children)) { // Article has more than just <actions />
+            for (var i = this.props.children.length; i > 0; i--) {
+                if (this.props.children[i].type === 'actions') {
+                    var actionsElement = this.props.children[i];
+                    actions = actions.concat(actionsElement.props.children);
+                    break;
+                }
+            }
+        } else if ('type' in this.props.children && this.props.children.type === 'actions') { // only one element is nested; it could be <actions />
+            var actionElements = this.props.children.props.children;
+            actions = actions.concat(Array.isArray(actionElements) ? actionElements : [actionElements]);
+        }
+
+        return actions;
+    }
     
 }
+
+class Action extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        var className = [
+            'action',
+            'position' in this.props && this.props.position
+        ].filter(Boolean).join(' ');
+
+        return <div className={ className }>{this.props.children}</div>;
+    }
+}
+
 
 export default Article;
