ea6ff094f056a8844b48627490170b8c05d086bc
Albert Villaroman <albert@the-social-edge.com>
2016-05-11 10:55:50 -0400

    show: datepicker on dashboard

--- app/js/components/shared/toolbar.js
+++ app/js/components/shared/toolbar.js
@@ -9,25 +9,11 @@ class Toolbar extends React.Component {
     constructor(props) {
         super(props);
 
-        this.defaults = {
-            scrollStrengthBuffer: 5 // the amount of pixels on the page the user must scroll thorugh before collapsing/revealing header
-        };
-
         let toolbarType = props.type || 'default'
 
         this.state = {
-            collapse: false,
-            previousDirection: false,
             type: toolbarType
         };
-    }
-
-    componentDidMount() {
-        document.body.addEventListener('mousewheel', this.slideUp.bind(this));
-    }
-
-    componentWillUnmount() {
-        document.body.removeEventListener('mousewheel', this.slideUp.bind(this));
     }
 
     /**
@@ -201,15 +187,6 @@ class Toolbar extends React.Component {
                     { this.generateToolbar() }
             </div>
         );
-    }
-
-    slideUp(e) {
-        return;
-        var currentDirection = e.deltaY > 0; // ? 'down' : 'up';
-
-        if (currentDirection !== this.state.previousDirection && e.deltaY !== 0) {
-            this.setState({ collapse: currentDirection, previousDirection: currentDirection });
-        }
     }
 
     getClassNames() { // TODO: require classnames module instead
