646928dd49817702e2861e78ad995036a7f8af8f
Ken Krug <ken@the-social-edge.com>
2016-05-13 16:45:18 -0400

    Added Freshdesk widget

--- app/js/components/shared/index.js
+++ app/js/components/shared/index.js
@@ -5,6 +5,7 @@ import GAInit from './googleAnalytics';
 import Container from './Container.component';
 import Drawer from './Drawer.component';
 import Main from './Main.component';
+import Freshdesk from './freshdesk';
 
 exports.Header = Header
 exports.Toolbar = Toolbar
@@ -12,4 +13,6 @@ exports.Facebook = Facebook
 exports.GAInit = GAInit
 exports.Container = Container;
 exports.Drawer = Drawer;
+exports.Freshdesk = Freshdesk;
 exports.Main = Main;
+
