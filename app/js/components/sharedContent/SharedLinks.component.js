00db8d2b4577e265868421c1eba0711930e181e1
James Tymann <jtymann@the-social-edge.com>
2016-05-19 16:20:17 -0400

    Switched over to fixed-data-tables

--- app/js/components/sharedContent/SharedLinks.component.js
+++ app/js/components/sharedContent/SharedLinks.component.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Table from 'react-toolbox/lib/table'
+import { Table, Column, Cell } from 'fixed-data-table';
 
 class SharedLinks extends React.Component {
 
@@ -20,7 +20,7 @@ class SharedLinks extends React.Component {
             type: String,
             title: 'URL'
         },
-        clicks: {
+        total_clicks: {
             type: Number,
             title: 'Clicks'
         },
@@ -36,17 +36,17 @@ class SharedLinks extends React.Component {
             type: Number,
             title: 'CPC'
         },
-        revenue: {
+        cost: {
             type: Number,
             title: 'Revenue'
         },
         saved_date: {
-            type: String,
+            type: Date,
             title: 'Saved'
         },
         published_at: {
-            type: Number,
-            title: 'Published At'
+            type: Date,
+            title: 'Published'
         },
 
     }
@@ -54,10 +54,93 @@ class SharedLinks extends React.Component {
     render() {
         return (
             <Table
-		        model = { this.dataModel }
-				selectable = { false }
-				source = { this.props.links }
-			/>
+                rowHeight={50}
+                rowsCount={this.props.links.length}
+                width={7000}
+                height={((this.props.links.length+1) * 50) + 2}
+                headerHeight={50}>
+                <Column
+                  header={<Cell>Title</Cell>}
+                  cell={({rowIndex, ...props}) => (
+                    <Cell {...props}>
+                        {this.props.links[rowIndex].title}
+                    </Cell>
+                  )}
+                  width={300}
+                />
+                <Column
+                  header={<Cell>Site</Cell>}
+                  cell={({rowIndex, ...props}) => (
+                    <Cell {...props}>
+                        {this.props.links[rowIndex].site_name}
+                    </Cell>
+                  )}
+                  width={200}
+                />
+                <Column
+                  header={<Cell>URL</Cell>}
+                  cell={({rowIndex, ...props}) => (
+                    <Cell {...props}>
+                        {this.props.links[rowIndex].shortlink}
+                    </Cell>
+                  )}
+                  width={200}
+                />
+                <Column
+                  header={<Cell>Clicks</Cell>}
+                  cell={({rowIndex, ...props}) => (
+                    <Cell {...props}>
+                        {this.props.links[rowIndex].total_clicks}
+                    </Cell>
+                  )}
+                  width={200}
+                />
+                <Column
+                  header={<Cell>Reach</Cell>}
+                  cell={({rowIndex, ...props}) => (
+                    <Cell {...props}>
+                        {this.props.links[rowIndex].fb_reach}
+                    </Cell>
+                  )}
+                  width={200}
+                />
+                <Column
+                  header={<Cell>CTR</Cell>}
+                  cell={({rowIndex, ...props}) => (
+                    <Cell {...props}>
+                        {this.props.links[rowIndex].ctr}
+                    </Cell>
+                  )}
+                  width={200}
+                />
+                <Column
+                  header={<Cell>CPC</Cell>}
+                  cell={({rowIndex, ...props}) => (
+                    <Cell {...props}>
+                        {this.props.links[rowIndex].cpc}
+                    </Cell>
+                  )}
+                  width={200}
+                />
+                <Column
+                  header={<Cell>Saved</Cell>}
+                  cell={({rowIndex, ...props}) => (
+                    <Cell {...props}>
+                        {this.props.links[rowIndex].saved_date}
+                    </Cell>
+                  )}
+                  width={200}
+                />
+                <Column
+                  header={<Cell>Published</Cell>}
+                  cell={({rowIndex, ...props}) => (
+                    <Cell {...props}>
+                        {this.props.links[rowIndex].fb_reach}
+                    </Cell>
+                  )}
+                  width={200}
+                />
+              </Table>
         );
     }
 }
