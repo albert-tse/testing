4fb061eec0c8a693ac4c5bd1a4ab21412bed7b16
Albert Villaroman <albert@the-social-edge.com>
2016-05-19 16:40:43 -0400

    added: app bar to Saved page; Color changed to purple only because React-Toolbox primary color is purple. We need to change that to a final color

--- app/js/components/saved/index.js
+++ app/js/components/saved/index.js
@@ -4,6 +4,8 @@ import Component from '../shared/List.component';
 import ListStore from '../../stores/List.store'
 import ListActions from '../../actions/List.action'
 
+import { AppBar, IconButton, Navigation } from 'react-toolbox';
+
 class Saved extends React.Component {
 
     constructor(props) {
@@ -15,20 +17,27 @@ class Saved extends React.Component {
     }
 
     render() {
-        return <AltContainer listName = "saved"
-        stores = {
-            {
-                list: (props) => {
-                    return {
-                        store: ListStore,
-                        value: ListStore.getSavedList()
-                    };
-                }
-            }
-        }
-        actions = { ListActions }
-        component = { Component }
-        />;
+        return (
+            <AltContainer   listName="saved"
+                            actions = { ListActions }
+                            stores = {{
+                                list: (props) => ({
+                                    store: ListStore,
+                                    value: ListStore.getSavedList()
+                                })
+                            }}>
+                <AppBar className="space-out">
+                    <Navigation type="horizontal">
+                        <h1 className="title">Saved</h1>
+                    </Navigation>
+                    <Navigation type="horizontal">
+                        <IconButton icon="bookmark_border" inverse />
+                        <IconButton icon="share" inverse />
+                    </Navigation>
+                </AppBar>
+                <Component />
+            </AltContainer>
+        );
     }
 }
 
