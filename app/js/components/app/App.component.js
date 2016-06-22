7c2a22d519c6b83d241739da04b0b5f61cf3d581
Ken Krug <ken@the-social-edge.com>
2016-06-22 15:18:36 -0400

    Added back Freshdesk

--- app/js/components/app/App.component.js
+++ app/js/components/app/App.component.js
@@ -28,6 +28,7 @@ export default class App extends React.Component {
                     <Notifications />
                     <Analytics />
                 </Panel>
+                <Freshdesk />
             </Layout>
         );
     }
