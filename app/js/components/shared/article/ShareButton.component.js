3a4400e985ef8e5a3c7fddc127d13621b61858b8
Albert Villaroman <albert@the-social-edge.com>
2016-07-18 09:49:31 -0400

    started: dropdown menu share

--- app/js/components/shared/article/ShareButton.component.js
+++ app/js/components/shared/article/ShareButton.component.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import { IconButton, Tooltip } from 'react-toolbox';
+import { IconMenu, MenuItem, Tooltip } from 'react-toolbox';
 import AltContainer from 'alt-container';
 import LinkStore from '../../../stores/Link.store';
 import LinkActions from '../../../actions/Link.action';
+import Styles from './styles';
 
 export default class ShareButton extends Component {
     constructor(props) {
         super(props);
     }
 
-    render() {  
+    render() {
+        return (
+            <IconMenu
+                icon="share"
+                position="auto"
+                className={Styles.showOnHover}
+                onClick={evt => evt.stopPropagation()}
+            >
+                <MenuItem value="copy" caption="Get Link" onClick={evt => console.log(evt)} />
+            </IconMenu>
+        );
+    }
+
+    /*
+    render() {
         const TooltipIconButton = Tooltip(IconButton);
         return (
             <TooltipIconButton
                 primary
+                className={Styles.showOnHover}
                 icon="share"
                 onClick={::this.onCopy}
                 tooltip="Get Link"
             />
         );
     }
+    */
 
     onCopy(evt) {
         LinkActions.generateLink(this.props.ucid);
