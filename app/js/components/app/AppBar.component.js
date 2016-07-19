b4db607c2f783b1fc273e3f37c9dc2f4dc4c5ad9
Albert Villaroman <albert@the-social-edge.com>
2016-07-19 16:01:21 -0400

    #163 User should be able to go to Explore page by clicking on logo

--- app/js/components/app/AppBar.component.js
+++ app/js/components/app/AppBar.component.js
@@ -16,7 +16,7 @@ export default class AppBar extends Component {
     render() {
         return (
             <ReactAppBar>
-                <h1 className={Styles.brand}>Contempo</h1>
+                <h1 className={Styles.brand} onClick={History.push.bind(this, Config.routes.explore)}>Contempo</h1>
                 <Navigation type="horizontal">
                     <Link label="Explore" icon="explore" active={/explore/.test(this.props.path)} onClick={History.push.bind(this, Config.routes.explore)} />
                     <Link label="Saved" icon="bookmark" active={/saved/.test(this.props.path)} onClick={History.push.bind(this, Config.routes.saved)} />
