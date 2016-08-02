a8ede4b134a5e3461e6735034618ccacb0583714
Ken Krug <ken@the-social-edge.com>
2016-08-02 16:01:52 -0400

    [ch240] Additional widgets for internal users

--- app/js/components/sharedContent/dashboard/Widgets.component.js
+++ app/js/components/sharedContent/dashboard/Widgets.component.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AltContainer from 'alt-container';
 import InfluencerStore from '../../../stores/Influencer.store';
+import UserStore from '../../../stores/User.store';
 import { widget, widgetContainer, widgetWrapper } from './style';
 import numeral from 'numeral';
 import moment from 'moment';
@@ -14,9 +15,20 @@ export default class Widgets extends React.Component {
     render() {
         return (
             <AltContainer
-                store={InfluencerStore}
+                stores={
+                    {
+                        InfluencerStore,
+                        UserStore
+                    }
+                }
                 component={Component}
-                transform={ ({searchSummary, projectedRevenue}) => ({...searchSummary, projectedRevenue})}
+                transform={ ({InfluencerStore, UserStore}) => {
+                    const stats = InfluencerStore.searchSummary;
+                    const projectedRevenue = InfluencerStore.projectedRevenue;
+                    const userRole = UserStore.user.role;
+
+                    return {...stats, projectedRevenue, userRole};
+                }}
             />
         );
     }
@@ -29,16 +41,47 @@ class Component extends React.Component {
     }
 
     render() {
-        const { estimatedRevenue, totalPosts, averageRevenuePerPost, projectedRevenue } = this.props;
+        const {
+            estimatedRevenue,
+            totalPosts,
+            averageRevenuePerPost,
+            totalClicks,
+            averageClicksPerPost,
+            averageCtrPerPost,
+            averageReachPerPost,
+            postsPerDay,
+            clicksPerDay,
+            reachPerDay,
+            projectedRevenue,
+            userRole
+        } = this.props;
 
         const projectedRevenueLabel = `Projected Revenue (${moment().format('MMM YYYY')})`;
 
+        let displayWidgets = [
+            <Widget key='1' label="Estimated Revenue" value={numeral(estimatedRevenue).format('$0,0.00')} />,
+            <Widget key='2' label="Total Posts" value={numeral(totalPosts).format('0.00a')} />,
+            <Widget key='3' label="Average Revenue per Post" value={numeral(averageRevenuePerPost).format('$0,0.00')} />,
+            <Widget key='4' label={projectedRevenueLabel} value={numeral(projectedRevenue).format('$0,0.00')} />,
+        ];
+
+        const internalWidgets = [
+            <Widget key='5' label="Total Clicks" value={numeral(totalClicks).format('0.00a')} />,
+            <Widget key='6' label="Average Clicks Per Post" value={numeral(averageClicksPerPost).format('0.00a')} />,
+            <Widget key='7' label="Average CTR Per Post" value={numeral(averageCtrPerPost).format('0.00a')} />,
+            <Widget key='8' label="Average Reach Per Post" value={numeral(averageReachPerPost).format('0.00a')} />,
+            <Widget key='9' label="Posts Per Day" value={numeral(postsPerDay).format('0.00a')} />,
+            <Widget key='10' label="Clicks Per Day" value={numeral(clicksPerDay).format('0.00a')} />,
+            <Widget key='11' label="Reach Per Day" value={numeral(reachPerDay).format('0.00a')} />,
+        ];
+
+        if (userRole === 'internal_influencer' || userRole === 'admin') {
+            displayWidgets = displayWidgets.concat(internalWidgets);
+        }
+
         return (
             <section className={widgetContainer}>
-                <Widget label="Estimated Revenue" value={numeral(estimatedRevenue).format('$0,0.00')} />
-                <Widget label="Total Posts" value={numeral(totalPosts).format('0.00a')} />
-                <Widget label="Average Revenue per Post" value={numeral(averageRevenuePerPost).format('$0,0.00')} />
-                <Widget label={projectedRevenueLabel} value={numeral(projectedRevenue).format('$0,0.00')} />
+                {displayWidgets}
             </section>
         );
     }
--- app/js/stores/Influencer.store.js
+++ app/js/stores/Influencer.store.js
@@ -6,7 +6,7 @@ import reduce from 'lodash/reduce';
 
 var BaseState = {
     searchedClickTotals: [],
-    searchedLinkTotals: [], 
+    searchedLinkTotals: [],
     searchSummary: null,
     projectedRevenue: 0,
     testInfluencers: {
@@ -53,19 +53,62 @@ class InfluencerStore {
         const totalPosts = links.data.links ? links.data.links.length : 0;
         const averageRevenuePerPost = estimatedRevenue / totalPosts;
 
+        const totalClicks = _.reduce(links.data.links, (total, link) => {
+            return total + link.total_clicks;
+        }, 0);
+
+        const totalReach = _.reduce(links.data.links, (total, link) => {
+            return total + link.fb_reach;
+        }, 0);
+
+        const averageClicksPerPost = totalClicks / totalPosts;
+
         links.data.links = links.data.links.map(link => Object.assign({}, link, {
             ctr: link.ctr || 0,
             fb_permalink: typeof link.fb_permalink === 'string' ? link.fb_permalink : '',
             fb_reach: link.fb_reach || 0,
-            shared_date: typeof(link.shared_date) === 'string' ? new Date(link.shared_date).getTime() : 0,
+            shared_date: typeof (link.shared_date) === 'string' ? new Date(link.shared_date).getTime() : 0,
         }));
+
+        let averageCtr = 0;
+
+        if (totalPosts > 0) {
+            let totalCtr = _.reduce(links.data.links, (total, link) => {
+                return total + link.ctr;
+            }, 0);
+
+            averageCtr = totalCtr / totalPosts;
+        }
+
+        let averageReach = 0;
+
+        if (totalPosts > 0) {
+            averageReach = totalReach / totalPosts;
+        }
+
+        let postsPerDay = totalPosts;
+        let clicksPerDay = totalClicks;
+        let reachPerDay = totalReach;
+
+        if (links.data.queriedDays > 0) {
+            postsPerDay = totalPosts / links.data.queriedDays;
+            clicksPerDay = totalClicks / links.data.queriedDays;
+            reachPerDay = totalReach / links.data.queriedDays;
+        }
 
         this.setState({
             searchedLinkTotals: links.data,
             searchSummary: Object.assign({}, this.searchSummary, {
                 estimatedRevenue: estimatedRevenue,
                 totalPosts: totalPosts,
-                averageRevenuePerPost: averageRevenuePerPost
+                averageRevenuePerPost: averageRevenuePerPost,
+                totalClicks: totalClicks,
+                averageClicksPerPost: averageClicksPerPost,
+                averageCtrPerPost: averageCtr,
+                averageReachPerPost: averageReach,
+                postsPerDay: postsPerDay,
+                clicksPerDay: clicksPerDay,
+                reachPerDay: reachPerDay
             })
         });
     }
