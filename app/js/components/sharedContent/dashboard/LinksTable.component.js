2361873543f591bc04c3efb2a2fa6408d5760f7d
Albert Villaroman <albert@the-social-edge.com>
2016-08-04 14:58:25 -0400

    added: infinite scrolling to links table

--- app/js/components/sharedContent/dashboard/LinksTable.component.js
+++ app/js/components/sharedContent/dashboard/LinksTable.component.js
@@ -6,7 +6,7 @@ import Griddle from 'griddle-react';
 import _ from 'lodash';
 import moment from 'moment';
 import numeral from 'numeral';
-import { headline, title, linkTable, linkRow, metadata, sortable, siteName } from './style';
+import { avatar, headline, title, linkTable, linkRow, metadata, sortable, siteName } from './style';
 
 export default class LinksTable extends React.Component {
 
@@ -42,7 +42,10 @@ class Component extends React.Component {
                     columns={['title', 'total_clicks', 'fb_reach', 'ctr', 'cpc']}
                     columnMetadata={columnMetadata}
                     results={links}
-                    resultsPerPage={50}
+                    resultsPerPage={25}
+                    enableInfiniteScroll={true}
+                    useFixedLayout={false}
+                    useFixedHeader={true}
                 />
             </div>
         );
@@ -53,13 +56,14 @@ class Component extends React.Component {
 const article = ({rowData}) => {
     return (
         <article className={linkRow}>
-            <header className={headline}>
+            <img className={avatar} src={rowData.influencer.fb_profile_image} />
+            <section className={headline}>
                 <p className={siteName}>{rowData.site_name}</p>
                 {rowData.title}
-            </header>
-            <small className={metadata}>
-                {rowData.influencer.name} - {rowData.platform.name} - {rowData.shared_date > 0 ? moment(rowData.shared_date).timeAgo() : 'Not Shared'}
-            </small>
+                <footer className={metadata}>
+                    {rowData.influencer.name} - {rowData.platform.name} - {rowData.shared_date > 0 ? moment(rowData.shared_date).timeAgo() : 'Not Shared'}
+                </footer>
+            </section>
         </article>
     );
 };
