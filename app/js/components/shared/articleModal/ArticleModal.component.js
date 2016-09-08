b9d68cb402bbc6204619e5326c905cf0619de96a
James Tymann <jtymann@the-social-edge.com>
2016-09-08 14:36:54 -0400

    Pretty Pretty Article Info Modal

--- app/js/components/shared/articleModal/ArticleModal.component.js
+++ app/js/components/shared/articleModal/ArticleModal.component.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ArticleModalStats from './ArticleModalStats.component';
+import SaveButton from '../article/SaveButton.component';
+import LinkStore from '../../../stores/Link.store';
+import LinkActions from '../../../actions/Link.action';
 import { Dialog } from 'react-toolbox';
+import Link from 'react-toolbox/lib/link';
 import moment from 'moment';
+import defer from 'lodash/defer';
 import Styles from './styles';
 
 /**
@@ -22,15 +27,24 @@ class ArticleModal extends React.Component {
             Styles.articleModal
         ].filter(Boolean).join(' ');
 
-        var publishedDate = article.publish_date ? moment(article.publish_date).format("MM/DD/YYYY hh:mma") : 'Unknown';
-
-        var articleFbCtr = hasStats(article) ? (<span>Average FB CTR: {article.averageFbCtr}%</span>) : '';
+        console.log(article);
 
         var articleLinkStats = !hasStats(article) ? (<p>Sorry, no stats are available for this article</p>) : article.links.map(function (link, index) {
             return (
-                <ArticleModalStats link={link} key={index}/>
+                <ArticleModalStats link={link} key={index} index={index}/>
             );
         });
+
+        var numLinks = article.links.length;
+        var clicks = _.reduce(article.links, function(acm, el){ 
+            if(el.stats.facebook && el.stats.facebook.clicks > 0){
+                acm += el.stats.facebook.clicks;
+            } else if(el.stats.post && el.stats.post.clicks > 0){
+                acm += el.stats.post.clicks;
+            }
+            return acm;
+        }, 0);
+        var fbCTR = article.averageFbCtr;
 
         return (
             <Dialog 
@@ -38,20 +52,38 @@ class ArticleModal extends React.Component {
                 active={this.props.visible}
                 className={classNames}
                 onOverlayClick={evt => ::this.hide()}>
+                
                 <div className={Styles.articleDetail}>
-                    <h3 className={Styles.articleTitle}><a href={article.url} target="_blank">{article.title}</a></h3>
-                    <img className={Styles.articleImage} src={article.image} />
-                    <p className={Styles.articleDescription}>
-                        <em>{article.site_name.toUpperCase()}</em> - {article.description}
-                        <p className={Styles.articlePublishDate}>
-                            Published: {publishedDate}
-                        </p>
-                    </p>
-                    <br style={{clear:'both'}} />
+                    <div className={Styles.articleImage}>
+                        <div style={{backgroundImage: 'url(' + article.image + ')'}}>
+                            <div className={Styles.saveButton}>
+                                <SaveButton ucid={article.ucid} />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={Styles.articleDescription}>
+                        <span className={Styles.siteName}>{article.site_name.toUpperCase()}</span>
+                        <span className={Styles.articlePublishDate}>
+                            {moment(article.publish_date).fromNow()}
+                        </span>
+                        <div className={Styles.articleTitle}>
+                            {article.title}<Link icon='open_in_new' href={article.url} target="_new" rel="nofollow"/>
+                        </div>
+                    </div>
+                    <br className={Styles.clear} />
+                </div>
+                <div className={Styles.totals}>
+                    <div className={Styles.totalsHeader}>Compiled Data</div>
+                    <ul>
+                        <li>Links: <span className={Styles.statValue}>{numLinks.toLocaleString()}</span></li>
+                        <li>Clicks: <span className={Styles.statValue}>{clicks.toLocaleString()}</span></li>
+                        <li>FB CTR: <span className={Styles.statValue}>{fbCTR.toLocaleString()}%</span></li>
+                    </ul>
+                    <div className={Styles.clear}></div>
                 </div>
                 <div className={Styles.linkStats}>
-                    <h1>Links</h1>
-                    {articleFbCtr}
+                    <h1>All Links ({numLinks})</h1>
                     {articleLinkStats}
                 </div>
             </Dialog>
--- app/js/components/shared/articleModal/ArticleModalStats.component.js
+++ app/js/components/shared/articleModal/ArticleModalStats.component.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Styles from './styles';
+import Styles from './stats';
 import moment from 'moment';
 
 class ArticleModalStats extends React.Component {
@@ -13,9 +13,11 @@ class ArticleModalStats extends React.Component {
         var link = this.props.link;
 
         var displayLink = link.shortlink.replace('po.st', 'qklnk.co');
-        var savedDate = link.saved_date ? moment(link.saved_date).format("MM/DD/YYYY hh:mma") : 'Unknown';
+        var savedDate = link.saved_date ? moment(link.saved_date).format("llll") : 'Unknown';
 
-        var fbStats = '';
+        var stats = '';
+        var fbStats = false;
+        var postStats = false;
 
         if (this.hasFbStats(link)) {
             var {
@@ -31,23 +33,38 @@ class ArticleModalStats extends React.Component {
             shareDate = shareDate ? moment(shareDate).format("MM/DD/YYYY hh:mma") : 'Unknown';
             
             fbStats = (
-                <div className={Styles.statBlock}>
-                    <span className={Styles.platform}>Facebook</span>
-                    <p>Share Date: {shareDate}</p>
-                    <p>Clicks: {parseInt(clicks).toLocaleString()}</p>
-                    <p>Reach: {parseInt(reach).toLocaleString()}</p>
-                    <p>CTR: {parseFloat(ctr).toLocaleString()}%</p>
-                    <p>Likes: {parseInt(likes).toLocaleString()}</p>
-                    <p>Shares: {parseInt(shares).toLocaleString()}</p>
-                    <p>Comments: {parseInt(comments).toLocaleString()}</p>
-                </div>
+                <ul className={Styles.statBlock + " " + Styles.fbStatBlock}>
+                    <li className={Styles.shareDate}>Facebook: <span className={Styles.statValue}>{shareDate}</span></li>
+                    <li>Clicks: <span className={Styles.statValue}>{parseInt(clicks).toLocaleString()}</span></li>
+                    <li>Reach: <span className={Styles.statValue}>{parseInt(reach).toLocaleString()}</span></li>
+                    <li>CTR: <span className={Styles.statValue}>{parseFloat(ctr).toLocaleString()}%</span></li>
+                    <li>Likes: <span className={Styles.statValue}>{parseInt(likes).toLocaleString()}</span></li>
+                    <li>Shares: <span className={Styles.statValue}>{parseInt(shares).toLocaleString()}</span></li>
+                    <li>Comments: <span className={Styles.statValue}>{parseInt(comments).toLocaleString()}</span></li>
+                </ul>
             );
         }
 
-        return (
-            <div className="influencer">
-                <h2>{link.influencer_name} - <a href={displayLink} target='_blank'>{displayLink}</a> - Created: {savedDate}</h2>
+        if(!fbStats && link.stats.post.clicks){
+            postStats = (
+                <ul className={Styles.statBlock + " " + Styles.postStatBlock}>
+                    <li>Clicks: <span className={Styles.statValue}>{parseInt(link.stats.post.clicks).toLocaleString()}</span></li>
+                </ul>
+            );
+        }
+
+        stats = (
+            <div>
                 {fbStats}
+                {postStats}
+            </div>
+        );
+
+        return (
+            <div className={Styles.link}>
+                <div>{this.props.index + 1}. {link.influencer_name}: - <a href={displayLink} target='_blank'>{displayLink}</a></div>
+                <div><span className={Styles.statValue}>Created {savedDate}</span></div>
+                {stats}
             </div>
         );
     }
