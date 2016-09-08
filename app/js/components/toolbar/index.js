7897d27977f5196ba8eb2147f6cab25edc9a7705
Ken Krug <ken@the-social-edge.com>
2016-09-08 13:25:17 -0400

    Collapse filter options into dropdown for Explore page

--- app/js/components/toolbar/index.js
+++ app/js/components/toolbar/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from 'react-toolbox';
 import Toolbar from './Toolbar.component';
+import MobileToolbar from './MobileToolbar.component';
 import SearchActions from '../../actions/Search.action';
 import InfluencerStore from '../../stores/Influencer.store';
 import InfluencerActions from '../../actions/Influencer.action';
@@ -26,9 +27,20 @@ import { ArticleSorter,
 
 
 const createToolbar = function (props) {
+
+    let mobileToolbar = false;
+
+    if (props.mobileCollapse) {
+        mobileToolbar = <MobileToolbar {...props} />
+    }
     return React.createClass({
         render: function () {
-            return <Toolbar {...props} />
+            return (
+                <div>
+                    <Toolbar {...props} />
+                    {mobileToolbar}
+                </div>
+                )
         }
     });
 };
@@ -45,6 +57,8 @@ exports.Toolbars = {
     }),
 
     Filter: createToolbar({
+        mobileCollapse: true,
+        mobileTitle: 'Filters',
         left: [
             <TopicFilter key="0" />,
             <ArticleSorter key="1" />,
