92b5aaabb472325e70c74b6605bf12c3d4f1a313
Ken Krug <ken@the-social-edge.com>
2016-09-12 17:16:43 -0400

    Revert "use: Button instead of IconButton for batch toolbar actions"
    This reverts commit 5b8835dad0e9d38084184c4a549703dc2abccedb.

--- app/js/components/toolbar/toolbar_components/SaveArticles.component.js
+++ app/js/components/toolbar/toolbar_components/SaveArticles.component.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-import { Button } from 'react-toolbox';
+import { IconButton } from 'react-toolbox';
 import FilterStore from '../../../stores/Filter.store';
 import FilterActions from '../../../actions/Filter.action';
 import ListActions from '../../../actions/List.action';
@@ -15,7 +15,8 @@ export default class SaveArticles extends Component {
     render() {
         return (
             <div title="Save Articles">
-                <Button icon="bookmark_border" label="Save all to My Posts" onClick={::this.saveSelectedArticlesToSavedList} /> 
+                <IconButton icon="bookmark_border" onClick={::this.saveSelectedArticlesToSavedList} /> 
+                <span className="toolbarLabel">Save all to My Posts</span>
             </div>
         );
     }
