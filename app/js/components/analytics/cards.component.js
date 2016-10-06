03ab4992b309e6e334bb99298320c28e9092fbad
Ken Krug <ken@the-social-edge.com>
2016-10-06 15:08:16 -0400

    [ch802] Hide revenue widgets from external influencers on the Dashboard

--- app/js/components/analytics/cards.component.js
+++ app/js/components/analytics/cards.component.js
@@ -29,20 +29,20 @@ export default class Cards extends Component {
         const projectedRevenueLabel = `Projected Revenue (${moment().format('MMM YYYY')})`;
 
         let displayWidgets = [
-            <Widget key='1' label="Estimated Revenue" value={estimatedRevenue && numeral(estimatedRevenue).format('$0,0.00')} />,
-            <Widget key='2' label="Total Posts" value={totalLinkCount && numeral(totalLinkCount).format('0a')} />,
-            <Widget key='3' label="Average Revenue per Post" value={averageRevenuePerPost && numeral(averageRevenuePerPost).format('$0,0.00')} />,
-            <Widget key='4' label={projectedRevenueLabel} value={projectedRevenue && numeral(projectedRevenue).format('$0,0.00')} />,
-            <Widget key='5' label="Total Clicks" value={totalClicks && numeral(totalClicks).format('0.00a')} />,
+            <Widget key='1' label="Total Posts" value={totalLinkCount && numeral(totalLinkCount).format('0a')} />,
+            <Widget key='2' label="Total Clicks" value={totalClicks && numeral(totalClicks).format('0.00a')} />,
+            <Widget key='3' label="Average Clicks Per Post" value={averageClicksPerPost && numeral(averageClicksPerPost).format('0.00a')} />,
+            <Widget key='4' label="Posts Per Day" value={postsPerDay && numeral(postsPerDay).format('0a')} />,
         ];
 
         const internalWidgets = [
-            <Widget key='6' label="Average Clicks Per Post" value={averageClicksPerPost && numeral(averageClicksPerPost).format('0.00a')} />,
-            <Widget key='7' label="Average CTR Per Post" value={averageCtrPerPost && numeral(averageCtrPerPost).format('0.00a%')} />,
-            <Widget key='8' label="Average Reach Per Post" value={averageReachPerPost && numeral(averageReachPerPost).format('0.00a')} />,
-            <Widget key='9' label="Posts Per Day" value={postsPerDay && numeral(postsPerDay).format('0a')} />,
-            <Widget key='10' label="Clicks Per Day" value={clicksPerDay && numeral(clicksPerDay).format('0.00a')} />,
-            <Widget key='11' label="Reach Per Day" value={reachPerDay && numeral(reachPerDay).format('0.00a')} />,
+            <Widget key='5' label="Average CTR Per Post" value={averageCtrPerPost && numeral(averageCtrPerPost).format('0.00a%')} />,
+            <Widget key='6' label="Average Reach Per Post" value={averageReachPerPost && numeral(averageReachPerPost).format('0.00a')} />,
+            <Widget key='7' label="Average Revenue per Post" value={averageRevenuePerPost && numeral(averageRevenuePerPost).format('$0,0.00')} />,
+            <Widget key='8' label="Clicks Per Day" value={clicksPerDay && numeral(clicksPerDay).format('0.00a')} />,
+            <Widget key='9' label="Reach Per Day" value={reachPerDay && numeral(reachPerDay).format('0.00a')} />,
+            <Widget key='10' label="Estimated Revenue" value={estimatedRevenue && numeral(estimatedRevenue).format('$0,0.00')} />,
+            <Widget key='11' label={projectedRevenueLabel} value={projectedRevenue && numeral(projectedRevenue).format('$0,0.00')} />
         ];
 
         if (userRole === 'internal_influencer' || userRole === 'admin') {
