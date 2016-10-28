53e0c76a25102cf82fc5f4f2c8649ee7d92d2041
James Tymann <jtymann@the-social-edge.com>
2016-10-28 16:04:05 -0400

    Made the landing page after signup the Home screen

--- app/js/app.js
+++ app/js/app.js
@@ -150,7 +150,7 @@ function renderContempo(){
         <Router history={hashHistory} createElement={creationIntercept}>
             <Route component={App}>
                 <Route path={Config.routes.default} component={Home} onEnter={permissions.isAuthenticated}></Route>
-                <Route path={Config.routes.success} component={Explore} onEnter={permissions.isAuthenticated} isFromSignUp={true}></Route>
+                <Route path={Config.routes.success} component={Home} onEnter={permissions.isAuthenticated} isFromSignUp={true}></Route>
                 <Route path={Config.routes.explore} component={Explore} onEnter={permissions.isAuthenticated}></Route>
                 <Route path={Config.routes.saved} component={Saved} onEnter={permissions.isAuthenticated}></Route>
                 <Route path={Config.routes.analytics} component={Analytics} onEnter={permissions.isAuthenticated}>
