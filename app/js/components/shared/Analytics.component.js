b68751eef513ef581ebbb237931014d639f94242
Albert Villaroman <albert@the-social-edge.com>
2017-01-04 18:50:04 -0500

    exclude: dev environment from pushing analytics stats to GTM

--- app/js/components/shared/Analytics.component.js
+++ app/js/components/shared/Analytics.component.js
@@ -9,11 +9,13 @@ export default class Analytics extends Component {
     }
 
     componentDidMount() {
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer', Config.googleAnalyticsTag);
+        if (Config.googleAnalyticsTag) {
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', Config.googleAnalyticsTag);
+        }
     }
 
     render() {
