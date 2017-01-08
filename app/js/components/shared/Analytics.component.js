5f13cd11ad87f78051c1b73b4e7d772d7f1f76db
Albert Villaroman <albert@the-social-edge.com>
2017-01-08 00:28:08 -0500

    added: cordova build process

--- app/js/components/shared/Analytics.component.js
+++ app/js/components/shared/Analytics.component.js
@@ -13,21 +13,23 @@ export default class Analytics extends Component {
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer', Config.googleAnalyticsTag);
         }
     }
 
     render() {
+        return null;
+        /*
         if (Config.debug) {
             return false;
         } else {
             return (
                 <noscript>
                     <iframe
-                        src={`//www.googletagmanager.com/ns.html?id=${Config.googleAnalyticsTag}`} 
-                        height={0} 
-                        width={0} 
+                        src={`//www.googletagmanager.com/ns.html?id=${Config.googleAnalyticsTag}`}
+                        height={0}
+                        width={0}
                         style={{
                             'display': 'none',
                             'visibility': 'hidden'
@@ -35,6 +37,7 @@ export default class Analytics extends Component {
                 </noscript>
             );
         }
+        */
     }
 }
 
--- app/js/components/shared/facebook.js
+++ app/js/components/shared/facebook.js
@@ -23,7 +23,7 @@ class Facebook extends React.Component {
             }
             js = d.createElement(s);
             js.id = id;
-            js.src = "//connect.facebook.net/en_US/sdk.js";
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
     }
