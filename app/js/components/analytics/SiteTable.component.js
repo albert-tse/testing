939f44ebab7cd463d90d902bf6d12e6834f72319
James Tymann <jtymann@the-social-edge.com>
2017-02-16 13:00:20 -0500

    Added reach to the Sites Budget table

--- app/js/components/analytics/SiteTable.component.js
+++ app/js/components/analytics/SiteTable.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Griddle from 'griddle-react';
+import _ from 'lodash';
 import { checkIfPinned } from './table.component';
 
 import { isMobilePhone } from '../../utils';
@@ -17,6 +18,20 @@ export default class SiteTable extends Component {
     render() {
         const isMobile = isMobilePhone();
 
+        let sortedData = _.orderBy(this.props.siteData, [ function(data){
+                if(data.siteBudget == -1){
+                    //All sites without a budget go to the bottom of the report, they will be sorted by clicks
+                    return -2;
+                }else if(data.siteBudget == 0){
+                    //This is an error state, that needs to be manually resolved by the accounts team
+                    //We put these above the general site list so that they stick out
+                    return -1;
+                }else{
+                    //Sort the rest by the % reached
+                    return data.siteClicks / data.siteBudget;
+                }
+            } ,"siteClicks"], ["desc","desc"]);
+
         return (
             <div className="griddle-container">
                 <div className="griddle-body">
@@ -28,12 +43,19 @@ export default class SiteTable extends Component {
                                     <th>Site</th>
                                     <th>Billable Clicks</th>
                                     <th>Click Cap</th>
+                                    <th>% Reached</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                {this.props.siteData.map((data, index) => {
+                                {sortedData.map((data, index) => {
                                     let clicks = data.siteClicks;
                                     let budget = data.siteBudget;
+                                    let reach = Math.round((clicks/budget)*100) + '%';
+                                    if(budget == 0){
+                                        reach = 'Invalid Budget';
+                                    } else if(budget == -1){
+                                        reach = ' -- ';
+                                    }
 
                                     let rowClassName = '';
 
@@ -50,6 +72,7 @@ export default class SiteTable extends Component {
                                             <td>{data.siteName}</td>
                                             <td>{numeral(clicks).format('0,0')}</td>
                                             <td>{budget !== -1 ? numeral(budget).format('0,0') : 'None'}</td>
+                                            <td>{reach}</td>
                                         </tr>
                                     )
                                 }
