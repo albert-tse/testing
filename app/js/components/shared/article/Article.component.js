64376fcbbee228176472c0904325afb356709003
Albert Villaroman <albert@the-social-edge.com>
2017-02-28 15:51:32 -0500

    bugfix: convert UTC timestamp of article creation date to local timezone

--- app/js/components/shared/article/Article.component.js
+++ app/js/components/shared/article/Article.component.js
@@ -73,7 +73,7 @@ export default class Article extends Component {
                         <div className={Styles.content}>
                             <div className={Styles.metadata}>
                                 <span className={Styles.site}>{article.site_name}{/*article.site_rating*/}</span>
-                                <time className={Styles.timeAgo} dateTime={moment(article.creation_date).format()}>{this.formatTimeAgo(article.creation_date)}</time>
+                                <time className={Styles.timeAgo} dateTime={moment(creationDate).format()}>{this.formatTimeAgo(creationDate)}</time>
                             </div>
                             <span className={Styles.headline}>
                                 <header data-score={article.clickbaitScore}>
