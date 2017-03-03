9e0d271f979b7cd12a68e485c5bf27ba2c56f996
Albert Villaroman <albert@the-social-edge.com>
2017-03-03 12:43:28 -0500

    bugfix: allow use to "Back" from scheduling

--- app/js/components/shared/article/ShareDialog.component.js
+++ app/js/components/shared/article/ShareDialog.component.js
@@ -321,7 +321,7 @@ class CustomDialog extends Component {
      * @param {Object} payload contains the selected date and whether or not it should schedule or just update
      */
     updateSelectedDate(payload) {
-        if (payload.selectedDate === null) {
+        if (payload && payload.selectedDate === null || !payload) {
             this.toggleScheduling();
         } else {
             const attachment = this.state.storyMetadata;
