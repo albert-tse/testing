6277704ba0aba308556553fd3ee2c84cbd9bcf57
Albert Villaroman <albert@the-social-edge.com>
2017-03-07 14:32:15 -0500

    skinned: log in page

--- app/js/components/login/login.component.js
+++ app/js/components/login/login.component.js
@@ -13,79 +13,43 @@ class LoginComponent extends Component {
 
     constructor(props) {
         super(props);
+        this.AuthOptions = this.AuthOptions.bind(this);
     }
 
     render() {
+        console.log(this.props);
         return (
             <div id="login" className={classnames(Styles.sendToBack, scrollable, vertical)}>
                 <Facebook />
                 <Analytics />
-                <div className='with-cover'>
-                    <div className={overlay}>
-                        <div className='container'>
-                            <div className={jumbotron}>
-                                <h1 className={Styles.brand}>Contempo</h1>
-                                <h2>Login / Signup</h2>
-                                <p>
-                                    Select one of the options below to sign in. Don't have an account? Just log in with any of the accounts below, and we will make one for you.
-                                </p>
-                                { this.renderAuthOptions() }
-                                { this.renderErrorMessage() }
-                                <span className={Styles.footnote} onClick={function(){ History.push(Config.routes.loginState.replace(':state', 'publisher')); }}> 
-                                    (Are you a publisher? Click here to login as a publisher.)
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className={classnames(container, 'container')}>
-                    <article className={classnames(Styles.article, 'row')}>
-                        <section className="col-xs-12 col-sm-6">
-                            <h2>Influencers</h2>
-                            <h3 className={Styles.heading}>Access diverse, high-quality paid content from a network of online publishers</h3>
-                            <p>Unlike typical influencer-publisher partnerships, The Social Edge’s lineup of online publishing houses offers a dynamic feed of content diverse enough to suit any fan base. Our publishers are high quality, vetted sources—many are sites you already love.</p>
-                            <h3 className={Styles.heading}> Inspire movements. Let us handle the logistics.</h3>
-                            <p>The Social Edge makes finding and sharing meaningful content with fans easy and enjoyable. Our expert team handles all the hoopla of contracts, tracking and payment, so you can spend energy on more interesting stuff — like building a social movement.</p>
-                            <h3 className={Styles.heading}>Stay true to your voice and grow your followers organically</h3>
-                            <p>You choose what material to share and when to share it. We help by giving you the best recommendations from a constant stream of new share-worthy articles, so you can keep your fans engaged with laughs, news and editorials that resonate with you.</p>
-                            <a href="http://the-social-edge.com/influencers" target="_blank">Learn More</a>
-                        </section>
-                        <section className="col-xs-12 col-sm-6">
-                            <h2>Publishers</h2>
-                            <h3 className={Styles.heading}>Increase traffic exponentially and attract new regular readership</h3>
-                            <p>Our broad influencer network broadcasts your content to millions of followers, plus millions more when followers share with their friends. With the exposure our Influencers can offer, you’ll become the go-to source for quality content in your industry.</p>
-                            <h3 className={Styles.heading}>Match your content with targeted, engaged audiences</h3>
-                            <p>Our approach capitalizes on the organic reach of vetted, influential internet personalities with avid followers who are eager to consume recommended content. Targeting specific fan bases yields more clicks than traditional and native ads.</p>
-                            <h3 className={Styles.heading}>Configure your content feed for effortless management</h3>
-                            <p>Spend less time distributing content and more time creating it. Our publisher portal automatically pulls content from your RSS feed and generates social media cards for our influencers to share. Additional levels of content management are also readily available.</p>
-                            <a href="http://the-social-edge.com/publishers" target="_blank">Learn More</a>
-                        </section>
-                    </article>
-                </div>
+                <h1 className={Styles.brand}>Contempo</h1>
+                <h2 className={Styles.heading}>Sign In</h2>
+                <this.AuthOptions />
+                <p className={Styles.message}>Not yet a member? <a href="//thesocialedge.co" target="_blank">Learn about Contempo</a></p>
+                <footer>
+                    <p className={Styles.disclaimer}>
+                        BY CREATING AN ACCOUNT, YOU ACKNOWLEDGE THAT YOU HAVE<br />
+                        READ AND ACCEPT THE SOCIAL EDGE’S <strong>TERMS OF SERVICE AND PRIVACY POLICY</strong>
+                    </p>
+                </footer>
+                { this.renderErrorMessage() }
                 { this.renderModalBackdrop() }
             </div>
         );
     }
 
-    renderAuthOptions() {
-        var ithis = this;
+    AuthOptions() {
         return (
-            <div id="auth-options">
-                { _.map(this.props.authTypes, function(el){
-                return <a onClick={ 
-                    function(){
-                        var role = ithis.props.route_state;
-                        var query = window.location.hash.split('?');
-                        query = query.length > 1 ? query[1] : '';
-                        query = qs.parse(query);
-
-                        if(query.ref){
-                            el.action(ithis.props.route_state,query.ref)
-                        }else{
-                            el.action(ithis.props.route_state,'unreferred')
-                        }
-                    }} key={ el.text } className={classnames(Styles.socialIcons, el.text.toLowerCase())}><i className={ 'fa fa-lg fa-' + el.text.toLowerCase() }></i></a>
-                }) }
+            <div id="auth-options" className={Styles.authOptions}>
+                {this.props.authTypes.map(el => (
+                    <a
+                        onClick={this.login.bind(this, el)}
+                        key={el.text}
+                        className={classnames(Styles.socialIcons, el.text.toLowerCase())}>
+                        <i className={classnames('fa fa-' + el.text.toLowerCase(), Styles.icon)}></i>
+                        <span>Continue with {el.text}</span>
+                    </a>
+                ))}
             </div>
         );
     }
@@ -113,7 +77,7 @@ class LoginComponent extends Component {
     }
 
     renderModalBackdrop() {
-        var classNames = 'modal-backdrop ';
+        let classNames = 'modal-backdrop ';
 
         if (this.props.authenticating) {
             classNames += ' fade in';
@@ -125,6 +89,25 @@ class LoginComponent extends Component {
             <div className={classnames('modal-backdrop', this.props.authenticating ? 'fade in' : 'hidden')}></div>
         );
     }
+
+    /**
+     * Log in via the specified auth option
+     * @param {Element} el the HTMLElement of the auth option
+     */
+    login(el) {
+        const role = this.props.route_state;
+        let query = window.location.hash.split('?');
+        query = query.length > 1 ? query[1] : '';
+        query = qs.parse(query);
+
+        if (query.ref) {
+            el.action(this.props.route_state,query.ref)
+        } else {
+            el.action(this.props.route_state,'unreferred')
+        }
+    }
+
+
 }
 
 export default LoginComponent;
