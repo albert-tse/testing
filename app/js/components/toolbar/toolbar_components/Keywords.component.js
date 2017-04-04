f1884e6a13357aeb5fdf10f66822d954bdf06b83
Albert Villaroman <albert@the-social-edge.com>
2017-04-04 12:04:45 -0400

    bugfix: debounce searching

--- app/js/components/toolbar/toolbar_components/Keywords.component.js
+++ app/js/components/toolbar/toolbar_components/Keywords.component.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import { IconButton, Input } from 'react-toolbox';
-import FilterStore from '../../../stores/Filter.store';
-import FilterActions from '../../../actions/Filter.action';
-import SearchStore from '../../../stores/Search.store';
-import SearchActions from '../../../actions/Search.action';
-import theme, { bordered, clearEntry, keywordsBox } from './styles.keywords';
 import classnames from 'classnames';
+import { debounce } from 'lodash';
+
+import FilterStore from '../../../stores/Filter.store';
+import SearchStore from '../../../stores/Search.store';
+
+import FilterActions from '../../../actions/Filter.action';
+import SearchActions from '../../../actions/Search.action';
+
+import theme, { bordered, clearEntry, keywordsBox } from './styles.keywords';
 import { pushEvent } from '../../shared/Analytics.component';
 
 export default class Keywords extends Component {
@@ -14,7 +18,7 @@ export default class Keywords extends Component {
     constructor(props) {
         super(props);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
-        this.performSearch = this.performSearch.bind(this);
+        this.performSearch = debounce(this.performSearch.bind(this), 500);
         this.update = this.update.bind(this);
         this.clearSearch = this.clearSearch.bind(this);
         this.state = {
@@ -43,14 +47,10 @@ export default class Keywords extends Component {
         );
     }
 
-    performSearch(evt, onClear) {
-        if (evt.key === 'Enter' || (typeof onClear !== 'undefined' && onClear)) {
-            FilterActions.update(this.state);
-            pushEvent(this.state);
-            SearchActions.getResults();
-        } else {
-            return false;
-        }
+    performSearch() {
+        FilterActions.update(this.state);
+        pushEvent(this.state);
+        SearchActions.getResults();
     }
 
     update(newValue, callback) {
@@ -65,7 +65,7 @@ export default class Keywords extends Component {
 
     clearSearch(evt) {
         this.update('', () =>
-            this.performSearch(evt, true)
+            this.performSearch(undefined, true)
         );
     }
 }
