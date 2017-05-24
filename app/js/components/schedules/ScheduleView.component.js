5cf9ed22c3bb83a8b29512c6c69aa218f69f24ff
Albert Villaroman <albert@the-social-edge.com>
2017-05-24 18:18:37 -0400

    added: schedule for week dummy

--- app/js/components/schedules/ScheduleView.component.js
+++ app/js/components/schedules/ScheduleView.component.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import classnames from 'classnames';
 import { compose, pure, withHandlers, withState } from 'recompose';
-import { Input } from 'react-toolbox';
+import { Button, IconButton, Input } from 'react-toolbox';
 import { AutoComplete } from 'antd';
+import moment from 'moment';
 
 import { AppContent } from '../shared';
 import ProfileSelector from '../multi-influencer-selector';
-import { columns, stretch } from '../common';
+import { columns, extraPadding, stretch } from '../common';
 import TimeZonePicker from '../timezone-picker';
+import Styles from './styles';
 
 /**
  * Pure component showing a single profile's schedule perferences
@@ -22,9 +25,43 @@ function ScheduleView({
     return (
         <div className={columns}>
             <ProfileSelector isPinned disableDisconnectedInfluencers />
-            <AppContent id="Schedules" className={stretch}>
+            <AppContent id="Schedules" className={classnames(stretch, extraPadding)}>
                 <TimeZonePicker timezone={selectedProfile && selectedProfile.timezone} />
+                <div className={Styles.scheduleManager}>
+                    <DayColumn day={1} />
+                    <DayColumn day={2} />
+                    <DayColumn day={3} />
+                    <DayColumn day={4} />
+                    <DayColumn day={5} />
+                    <DayColumn day={6} />
+                    <DayColumn day={7} />
+                </div>
+                <Button icon="add" label="Add Timeslot" raised accent />
             </AppContent>
+        </div>
+    );
+}
+
+function DayColumn({
+    day
+}) {
+    return (
+        <div className={Styles.dayColumn}>
+            <h1 className={Styles.dayColumnHeader}>{moment().day(day).format('dddd')}</h1>
+            <ul className={Styles.timeSlotsList}>
+                <li className={Styles.timeSlotRow}>
+                    <div className={Styles.timeSlot}>
+                        <IconButton icon="clear" />
+                        <p className={Styles.timeSlotTime}>6:00 PM</p>
+                    </div>
+                </li>
+                <li className={Styles.timeSlotRow}>
+                    <div className={Styles.timeSlot}>
+                        <IconButton icon="clear" />
+                        <p className={Styles.timeSlotTime}>9:04 PM</p>
+                    </div>
+                </li>
+            </ul>
         </div>
     );
 }
