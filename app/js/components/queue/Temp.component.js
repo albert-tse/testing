b13483346517f296680a5e1fc7d230c8d86a1a58
Albert Villaroman <albert@the-social-edge.com>
2017-08-09 16:27:31 -0400

    refactoring: Calendar Queue view

---
+++ app/js/components/queue/Temp.component.js
@@ -0,0 +1,158 @@
+class TempComponent extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        //props.reloadPosts();
+
+        this.state = {
+            numberOfWeeks: 1,
+            today: moment.tz('UTC'),
+            scheduledPosts: [],
+            loadMore: ::this.loadMore
+        };
+
+        this.state = extend(this.state, this.convertPropsToState(props));
+    }
+
+    componentWillReceiveProps(nextProps){
+        if(this.props.SelectedProfile != nextProps.SelectedProfile){
+            this.setState({ numberOfWeeks: 1 });
+        }
+
+        this.setState(this.convertPropsToState(nextProps));
+    }
+
+    convertPropsToState(props, numWeeksOverride){
+        var scheduledPosts = props.ScheduledPosts.posts;
+
+        var state = {};
+
+        var queues = [];
+        var startDate = this.state.today.tz(props.SelectedProfile.timezone).startOf('day');
+
+        var numberOfWeeks = this.state.numberOfWeeks;
+        if(numWeeksOverride){
+            numberOfWeeks = numWeeksOverride;
+            state.numberOfWeeks = numWeeksOverride;
+        }
+
+        for(var i=0; i<(numberOfWeeks*DAYS_IN_A_WEEK); i++){
+            var date = startDate.clone().add(i, 'days');
+            var endOfDate = date.clone().add(1,'day');
+
+            var slots = props.SelectedProfile.slots[date.day()];
+            if(slots){
+                slots = slots.slice(0);
+            } else {
+                slots = [];
+            }
+
+            slots = slots.map((slot) => {
+                var newSlot = extend({}, slot);
+                newSlot.time = date.clone().hour(newSlot.time.hour()).minute(newSlot.time.minute());
+                return newSlot;
+            });
+
+            queues[i] = {
+                date: date,
+                timeslots: slots,
+                scheduledPosts: filter(scheduledPosts, function(post){
+                    post.time.tz(props.SelectedProfile.timezone);
+                    return post.time >= date && post.time < endOfDate;
+                })
+            }
+        }
+
+        //filter out any old posts or time slots that happened today
+        if(queues.length > 0){
+            queues[0].timeslots = filter(queues[0].timeslots, function(slot){
+                return slot.time > moment.tz(props.SelectedProfile.timezone);
+            });
+            queues[0].scheduledPosts = filter(queues[0].scheduledPosts, function(post){
+                return post.time > moment.tz(props.SelectedProfile.timezone);
+            });
+        }
+
+        state.queues = queues;
+
+        return state;
+    }
+
+    render() {
+        const {
+            loadMore,
+            ScheduledPosts,
+            SelectedProfile,
+            mini,
+            onDeleteCall
+        } = this.props;
+
+        const {
+            queues
+        } = this.state;
+
+        const {
+            loading,
+            posts,
+            slots,
+            totalScheduledPostsAmount
+        } = ScheduledPosts;
+
+        const hasScheduledPostsWithinDateRange = Array.isArray(posts) ? posts.length : 0;
+        const hasScheduledPostsOverall = totalScheduledPostsAmount > 0;
+        const hasTimeslots = Object.keys(SelectedProfile.slots).length;
+
+        let CallToAction = props => <div />
+
+        if (!hasTimeslots) {
+            if (hasScheduledPostsOverall) { // Scenarios 3 and 4: Posts have been scheduled but no timeslots
+                CallToAction = CTAToEditSchedule;
+            } else { // Scenario 2: Influencer has neither scheduled any posts nor defined timeslots
+                CallToAction = CTAToSchedulePostOrDefineTimeslots;
+            }
+        }
+
+        return (
+            <div className={classnames(Styles.queueContainer, mini && Styles.mini)}>
+                {!mini && <p className={Styles.scheduledPostAmount}>You have <strong>{totalScheduledPostsAmount}</strong> scheduled posts</p>}
+                {loading ? <this.Loading /> : (
+                    <div>
+                        <CallToAction mini={mini} />
+                        {(hasTimeslots || hasScheduledPostsOverall) && (
+                            <div>
+                                {map(queues, (queue, index) => (
+                                    <QueueItemCollection
+                                        key={index}
+                                        queue={queue}
+                                        mini={mini}
+                                        selectedProfile={SelectedProfile}
+                                        onDeleteCall={onDeleteCall}
+                                    />
+                                ))}
+                                <div className={classnames(!mini && Styles.mini, Styles.loadMoreContainer)}>
+                                    <Button className={Styles.loadMoreButton} raised accent label="Next Week" onClick={this.state.loadMore} />
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                )}
+            </div>
+        );
+    }
+
+    loadMore(){
+        let selectedProfile = this.props.SelectedProfile;
+
+        if (selectedProfile) {
+
+            let start = moment.tz(this.state.today, selectedProfile.timezone).startOf('day');
+            let end = moment.tz(this.state.today, selectedProfile.timezone).add(this.state.numberOfWeeks + 1, 'weeks').endOf('day');
+
+            defer(this.props.getScheduledPosts, selectedProfile.id, start, end);
+        }
+
+        var newState = this.convertPropsToState(this.props, this.state.numberOfWeeks + 1);
+        this.setState(newState);
+    }
+
+}
--- app/js/components/queue/index.js
+++ app/js/components/queue/index.js
@@ -1,51 +1,176 @@
-import React, { Component } from 'react';
-import AltContainer from 'alt-container';
-import { compose, pure, setPropTypes } from 'recompose';
+import React from 'react'
+import connect from 'alt-utils/lib/connect'
 import moment from 'moment-timezone';
-import extend from 'lodash/extend';
-import filter from 'lodash/filter';
-import PropTypes from 'prop-types';
+import _ from 'lodash';
 
-import ScheduledPostSource from '../../sources/ScheduledPost.source';
-import ScheduledPostStore from '../../stores/ScheduledPost.store';
-import ProfileSelectorStore from '../../stores/ProfileSelector.store';
-import FilterStore from '../../stores/Filter.store';
+import ProfileSelectorStore from '../../stores/ProfileSelector.store'
+import ScheduledPostStore from '../../stores/ScheduledPost.store'
 
-import FilterActions from '../../actions/Filter.action';
-import ScheduledPostActions from '../../actions/ScheduledPost.action';
+import ScheduledPostActions from '../../actions/ScheduledPost.action'
 
-import QueueComponent from './Queue.component';
+import QueueComponent, { Loading } from './Queue.component'
+import CTAToEditSchedule from '../null-states/CTAToEditSchedule.component';
+import CTAToSchedulePostOrDefineTimeslots from '../null-states/CTAToSchedulePostOrDefineTimeslots.component';
 
-const fetchScheduledPosts = ScheduledPostSource.getPosts().remote;
+const DAYS_IN_A_WEEK = 7;
 
-export default class Queue extends Component {
+class QueueContainer extends React.Component {
+
+    static initialState = {
+        numberOfWeeks: 1,
+        today: moment.tz('UTC'),
+        queues: []
+    }
+
+    state = {
+        ...QueueContainer.initialState
+        // scheduledPosts: [],
+        // loadMore: ::this.loadMore
+    }
+
+    componentDidMount() {
+        _.defer(this.fetchScheduledPosts, this.props.selectedProfile, this.state.today, this.numberOfWeeks)
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState === this.state) {
+            if (prevProps.selectedProfile !== this.props.selectedProfile) { // selected a different profile
+                this.setState(this.resetView)
+                this.fetchScheduledPosts(this.props.selectedProfile, this.state.today, this.state.numberOfWeeks)
+            }
+
+            if (prevProps.posts !== this.props.posts) { // loaded new set of posts
+                this.setState(this.groupPostsIntoQueues)
+            }
+        }
+    }
 
     render() {
-        var comp = this;
-
-        var stores = {
-            SelectedProfile:  function (props) {
-                return {
-                    store: ProfileSelectorStore,
-                    value: ProfileSelectorStore.getState().selectedProfile
-                };
-            },
-            ScheduledPosts: ScheduledPostStore,
-            Filters: FilterStore
-        };
-
-        var injected = {
-            mini: this.props.mini == true
-        }
-
-        return (
-            <AltContainer
-                inject={injected}
-                stores={stores}
-                actions={ScheduledPostActions}
-                component={QueueComponent}
+        return this.props.loading ? <Loading /> : (
+            <QueueComponent
+                mini={this.props.mini}
+                queues={this.getQueues(this.props, this.state)}
+                selectedProfile={this.props.selectedProfile}
+                totalScheduledPostsAmount={this.props.totalScheduledPostsAmount}
+                CallToAction={this.getCallToAction(_.pick(this.props, 'posts', 'totalScheduledPostsAmount', 'selectedProfile'))}
             />
         )
     }
 
+    createQueue = (date, posts, slots, timezone) => offsetInDays => {
+        // get date for current queue
+        const { start, end } = this.getDateRange(date, offsetInDays)
+
+        // get slots set for this day of the week
+        const timeslots = this.getProfileTimeslotsForDayOfWeek(slots, start)
+
+        // define queue properties
+        let queue = {
+            date,
+            timeslots,
+            scheduledPosts: _.filter(posts, post => {
+                const scheduledPostTime = post.time.tz(timezone)
+                return start <= scheduledPostTime && scheduledPostTime < end
+            })
+        }
+
+        // filter out any old posts or time slots that happened today
+        if (offsetInDays === 0) {
+            const timeNow = moment.tz(timezone)
+            queue = {
+                ...queue,
+                timeslots: _.filter(queue.slots, slot => slot.time > timeNow),
+                scheduledPosts: _.filter(queue.scheduledPosts, post => post.time > timeNow)
+            }
+        }
+
+        return queue
+    }
+
+    fetchScheduledPosts(selectedProfile, from, numberOfWeeks) {
+        const start = moment.tz(from, selectedProfile.timezone).startOf('day')
+        const end = start.clone().add(numberOfWeeks, 'weeks').endOf('day')
+        return ScheduledPostActions.getScheduledPosts(selectedProfile.id, start, end)
+    }
+
+    getCallToAction(props) {
+        const hasScheduledPostsWithinDateRange = Array.isArray(props.posts) ? props.posts.length : 0;
+        const hasScheduledPostsOverall = props.totalScheduledPostsAmount > 0;
+        const hasTimeslots = Object.keys(props.selectedProfile.slots).length;
+
+        if (!hasTimeslots) {
+            if (hasScheduledPostsOverall) { // Scenarios 3 and 4: Posts have been scheduled but no timeslots
+                return CTAToEditSchedule
+            } else { // Scenario 2: Influencer has neither scheduled any posts nor defined timeslots
+                return CTAToSchedulePostOrDefineTimeslots
+            }
+        } else {
+            return props => React.createElement('div')
+        }
+    }
+
+    getDateRange(startDate, dayOffset) {
+        const start = startDate.clone().add(dayOffset, 'days')
+        const end = start.clone().add(1, 'day')
+        return { start, end }
+    }
+
+    getProfileTimeslotsForDayOfWeek(timeslots = {}, day) {
+        const slots = timeslots[day.day()] || []
+        return _.map(slots, slot => {
+            return {
+                ...slot,
+                time: day.clone().hour(slot.time.hour()).minute(slot.time.minute())
+            }
+        })
+    }
+
+    getQueues(props, state) {
+        if (Object.keys(props.selectedProfile.slots).length > 0 || props.totalScheduledPostsAmount > 0) {
+            return state.queues
+        } else {
+            return []
+        }
+    }
+
+    groupPostsIntoQueues(state, props) {
+        const date = state.today.tz(props.selectedProfile.timezone).startOf('day')
+        const numDaysToShow = state.numberOfWeeks * DAYS_IN_A_WEEK
+        const { slots, timezone } = props.selectedProfile
+
+        let queues = _.times(numDaysToShow, this.createQueue(date, props.posts, slots, timezone))
+
+        return {
+            ...state,
+            queues
+        }
+    }
+
+    resetView(prevState, props) {
+        return { numberOfWeeks: 1 }
+    }
+
 }
+
+export default connect({
+    listenTo(props) {
+        return [ProfileSelectorStore, ScheduledPostStore]
+    },
+
+    reduceProps(props) {
+        const {
+            loading,
+            posts,
+            totalScheduledPostsAmount
+        } = ScheduledPostStore.getState()
+
+        return {
+            ...props, // TODO specify each property explicitly so we know what to compare
+            loading,
+            posts,
+            selectedProfile: ProfileSelectorStore.getState().selectedProfile,
+            totalScheduledPostsAmount
+        }
+    },
+
+})(QueueContainer)
