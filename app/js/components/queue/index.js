718fc680b89ffef790cf789273e6f5662238c6c5
Albert Villaroman <albert@the-social-edge.com>
2017-08-10 15:35:11 -0400

    added: documentation to QueueContainer

--- app/js/components/queue/index.js
+++ app/js/components/queue/index.js
@@ -27,10 +27,19 @@ class QueueContainer extends React.Component {
         ...QueueContainer.initialState
     }
 
+    /**
+     * Loads scheduled posts once the component is ready
+     */
     componentDidMount() {
         this.fetchScheduledPosts(this.props, this.state)
     }
 
+    /**
+     * Updates component state and may dispatch actions from here depending on which portion of
+     * the component changes
+     * @param {object} prevProps previous component properties
+     * @param {object} prevState previous component state
+     */
     componentDidUpdate(prevProps, prevState) {
         if (prevState === this.state) {
             if (prevProps.selectedProfile !== this.props.selectedProfile) { // selected a different profile
@@ -44,6 +53,10 @@ class QueueContainer extends React.Component {
         }
     }
 
+    /**
+     * This should only return the wrapped component along with explicitly specified
+     * properties it should have so that it will only update when necessary
+     */
     render() {
         return (
             <QueueComponent
@@ -59,6 +72,95 @@ class QueueContainer extends React.Component {
         )
     }
 
+
+    /**
+     * Makes a request for the latest scheduled posts given the current date range
+     * @param {object} props component properties
+     * @param {object} state component state
+     */
+    fetchScheduledPosts(props, state) {
+        const { selectedProfile } = props
+        const { numberOfWeeks, today } = state
+        const start = moment.tz(today, selectedProfile.timezone).startOf('day')
+        const end = start.clone().add(numberOfWeeks, 'weeks').endOf('day')
+
+        return _.defer(ScheduledPostActions.getScheduledPosts, selectedProfile.id, start, end)
+    }
+
+    /**
+     * Returns a component that the wrapped component can display if the user needs to take a certain action
+     * for instance: setting up scheduled time slots or scheduling a new post
+     * @param {object} props component props
+     * @return {React.Component|boolean}
+     */
+    getCallToAction(props) {
+        const hasScheduledPostsWithinDateRange = Array.isArray(props.posts) ? props.posts.length : 0;
+        const hasScheduledPostsOverall = props.totalScheduledPostsAmount > 0;
+        const hasTimeslots = Object.keys(props.selectedProfile.slots).length;
+
+        if (!hasTimeslots) {
+            if (hasScheduledPostsOverall) { // Scenarios 3 and 4: Posts have been scheduled but no timeslots
+                return CTAToEditSchedule
+            } else { // Scenario 2: Influencer has neither scheduled any posts nor defined timeslots
+                return CTAToSchedulePostOrDefineTimeslots
+            }
+        } else {
+            return false
+        }
+    }
+
+    /**
+     * Returns the start time of day and start time of next day
+     * @param {moment} startDate the date to get start time of
+     * @param {integer} dayOffset set this to non-zero if you want the start date to be n number of days
+     * @return {object}
+     */
+    getDateRange(startDate, dayOffset = 0) {
+        const start = startDate.clone().add(dayOffset, 'days')
+        const end = start.clone().add(1, 'day')
+        return { start, end }
+    }
+
+    /**
+     * Given a spec of timeslots for any given day of the week (represented as an integer),
+     * return the array of timeslots for the specified day
+     * @param {object} timeslots specifies set of timeslots for each day of the week (in integer)
+     * @param {integer} day of the week
+     * @return {array<object>}
+     */
+    getProfileTimeslotsForDayOfWeek(timeslots = {}, day) {
+        const slots = timeslots[day.day()] || []
+        return _.map(slots, slot => {
+            return {
+                ...slot,
+                time: day.clone().hour(slot.time.hour()).minute(slot.time.minute())
+            }
+        })
+    }
+
+    /**
+     * Returns an empty array if selected profile neither has timeslots nor scheduled posts
+     * @param {object} props component prop containing the selected profile's timeslots
+     * @param {object} state contains
+     * @return {array}
+     */
+    getQueues(props, state) {
+        if (Object.keys(props.selectedProfile.slots).length > 0 || props.totalScheduledPostsAmount > 0) {
+            return state.queues
+        } else {
+            return []
+        }
+    }
+
+    /**
+     * Combines the selected profile's timeslots with scheduled posts fetched from the server
+     * and return an array of Queue objects wherein each object is a combination of empty timeslots and scheduled posts for a given day of the week
+     * @param {moment} date the starting date for current view
+     * @param {array} posts scheduled posts fetched from server
+     * @param {string} timezone selected profile's timezone
+     * @param {integer} offsetInDays how many days from starting date is used to determine what next day is
+     * @return {object}
+     */
     createQueue = (date, posts, slots, timezone) => offsetInDays => {
         // get date for current queue
         const { start, end } = this.getDateRange(date, offsetInDays)
@@ -89,55 +191,13 @@ class QueueContainer extends React.Component {
         return queue
     }
 
-    fetchScheduledPosts(props, state) {
-        const { selectedProfile } = props
-        const { numberOfWeeks, today } = state
-        const start = moment.tz(today, selectedProfile.timezone).startOf('day')
-        const end = start.clone().add(numberOfWeeks, 'weeks').endOf('day')
-
-        return _.defer(ScheduledPostActions.getScheduledPosts, selectedProfile.id, start, end)
-    }
-
-    getCallToAction(props) {
-        const hasScheduledPostsWithinDateRange = Array.isArray(props.posts) ? props.posts.length : 0;
-        const hasScheduledPostsOverall = props.totalScheduledPostsAmount > 0;
-        const hasTimeslots = Object.keys(props.selectedProfile.slots).length;
-
-        if (!hasTimeslots) {
-            if (hasScheduledPostsOverall) { // Scenarios 3 and 4: Posts have been scheduled but no timeslots
-                return CTAToEditSchedule
-            } else { // Scenario 2: Influencer has neither scheduled any posts nor defined timeslots
-                return CTAToSchedulePostOrDefineTimeslots
-            }
-        } else {
-            return false
-        }
-    }
-
-    getDateRange(startDate, dayOffset) {
-        const start = startDate.clone().add(dayOffset, 'days')
-        const end = start.clone().add(1, 'day')
-        return { start, end }
-    }
-
-    getProfileTimeslotsForDayOfWeek(timeslots = {}, day) {
-        const slots = timeslots[day.day()] || []
-        return _.map(slots, slot => {
-            return {
-                ...slot,
-                time: day.clone().hour(slot.time.hour()).minute(slot.time.minute())
-            }
-        })
-    }
-
-    getQueues(props, state) {
-        if (Object.keys(props.selectedProfile.slots).length > 0 || props.totalScheduledPostsAmount > 0) {
-            return state.queues
-        } else {
-            return []
-        }
-    }
-
+    /**
+     * Updates component state by creating new set of queues given recently loaded scheduled posts
+     * or selected profile's timeslots
+     * @param {object} state previous component state
+     * @param {object} props component props
+     * @return {object}
+     */
     groupPostsIntoQueues(state, props) {
         const date = state.today.tz(props.selectedProfile.timezone).startOf('day')
         const numDaysToShow = state.numberOfWeeks * DAYS_IN_A_WEEK
@@ -153,7 +213,10 @@ class QueueContainer extends React.Component {
         }
     }
 
-    // TODO: append to state.queues rather than recreating everything
+    /**
+     * Update the number of days to show for current queue
+     * and fetch scheduled posts for those given days
+     */
     loadMore = () => {
         const { selectedProfile } = this.props
 
@@ -164,6 +227,12 @@ class QueueContainer extends React.Component {
         }
     }
 
+    /**
+     * Reset the component state back to initial when changing selected profile
+     * @param {object} prevState previous component state
+     * @param {object} props component props
+     * @return {object}
+     */
     resetView(prevState, props) {
         return {
             loading: true,
@@ -172,6 +241,12 @@ class QueueContainer extends React.Component {
         }
     }
 
+    /**
+     * Increments the number of weeks to show in current view by one
+     * @param {object} prevState previous component state
+     * @param {object} props component props
+     * @return {object}
+     */
     incrementNumberOfWeeks(prevState, props) {
         return { numberOfWeeks: prevState.numberOfWeeks + 1 }
     }
@@ -200,10 +275,22 @@ class QueueContainer extends React.Component {
 }
 
 export default connect({
+
+    /**
+     * Defines which stores the component should listen to for updates
+     * @param {object} props component props
+     * @return {array}
+     */
     listenTo(props) {
         return [ProfileSelectorStore, ScheduledPostStore]
     },
 
+    /**
+     * Defines the properties to give to the container based on the
+     * latest data obtained from store
+     * @param {object} props component props
+     * @return {object}
+     */
     reduceProps(props) {
         const {
             loading,
