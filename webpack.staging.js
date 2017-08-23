3ba4c0d248160498c1889262081d4eb8c6d4e68e
Albert Villaroman <albert@the-social-edge.com>
2017-08-23 18:29:43 -0400

    bugfix: use plugins from config on staging

--- webpack.staging.js
+++ webpack.staging.js
@@ -5,7 +5,7 @@ var path = require('path');
 process.env.NODE_ENV = 'staging';
 
 module.exports = Object.assign({}, config, {
-    plugins: plugins.slice(0, 5).concat([
+    plugins: config.plugins.slice(0, 5).concat([
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify('staging')
